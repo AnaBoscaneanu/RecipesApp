8073d681cfba8fb8f1b78399e3c44f14ea52809f
Ana Boscaneanu <ana_boscaneanu@yahoo.com>
2020-12-12 18:27:48 +0100

    Connect PostgreSQL db to run in production

--- app_files/__init__.py
+++ app_files/__init__.py
@@ -1,3 +1,4 @@
+from os import environ
 import flask
 
 from app_files.models import db
@@ -5,7 +6,7 @@ from app_files.models import db
 app = flask.Flask(__name__)
 
 # Configure the app to use SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipe_app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///recipe_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db.init_app(app)
